c75d9db2a676a44256911d6dc8a939807fc72eb2
bassamer <bassamer@fit.cvut.cz>
2025-08-01 19:03:57 +0200

    getindex

--- main.cpp
+++ main.cpp
@@ -9,10 +9,28 @@ constexpr int cell_size = 20;
 constexpr int rows_colls = 38;
 constexpr int window_size = 760;
 
+
+/**
+ * function for getting 2d index into 1d index
+ * @param x coord
+ * @param y coord
+ * @return index of the cell inside the graph
+ */
+int getIndex(int row, int col)
+{
+    return row * rows_colls + col;
+}
+
+
+
+
 int main() {
     InitWindow(window_size, window_size, "Mazey Mazeey");
     SetTargetFPS(60);
 
+
+    std::vector<CCell> grid;
+    std::stack<int> dfs_stack;
 
 
 
