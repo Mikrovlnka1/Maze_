8bfe523bf8f0c6f139ff4a87fb081ee81e5ddc51
bassamer <bassamer@fit.cvut.cz>
2025-08-01 18:48:42 +0200

    just blank screen and main loop

--- main.cpp
+++ main.cpp
@@ -5,11 +5,28 @@
 #include <vector>
 #include "CCell.h"
 
+constexpr int cell_size = 20;
+constexpr int rows_colls = 38;
+constexpr int window_size = 760;
 
 int main() {
+    InitWindow(window_size, window_size, "Mazey Mazeey");
+    SetTargetFPS(60);
 
 
 
 
+
+
+
+
+    while(!WindowShouldClose()){
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
